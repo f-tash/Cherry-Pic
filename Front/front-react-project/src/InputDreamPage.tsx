964d5adc135e431aec79c71519c7e257301678c6
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-26 19:33:24 +0900

    add: 画像を生成するボタンを追加

--- Front/front-react-project/src/InputDreamPage.tsx
+++ Front/front-react-project/src/InputDreamPage.tsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import SakuraCardArea from "./SakuraCardArea.tsx";
 
 const InputDreamPage: React.FC = () => {
   return (
-    <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      marginTop={10}
+    >
       <SakuraCardArea />
+      <Button
+        variant="contained"
+        sx={{
+          bgcolor: "#FF75B6",
+          color: "white",
+          paddingX: 5,
+          borderRadius: 15,
+          marginTop: 5,
+        }}
+      >
+        画像を生成する
+      </Button>
     </Box>
   );
 };
