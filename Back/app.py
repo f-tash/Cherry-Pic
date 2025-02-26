387a08a8a716b625e89fabfc546c187c8b785576
Keisuke Kimura <kimurakeisukej@gmail.com>
2025-02-26 18:34:53 +0900

    add: データ検証

--- Back/app.py
+++ Back/app.py
@@ -13,27 +13,25 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 @app.route("/dreams", methods=["GET"])
 def get_dreams():
     response = get_dreams_from_db()
     return jsonify(response)
 
+
 @app.route("/dreams", methods=["POST"])
 def post_dream():
     data = request.json
-    is_request_ok=validate_post_dreams(data)
-    if not is_request_ok :
+    is_request_ok = validate_post_dreams(data)
+    if not is_request_ok:
         return jsonify({"error": "Bad Request"}), 400
     dream_title = data["dream_title"]
-    dream_img_url = data["url"]
-    
-    response = {
-        "message": "dream created successfully",
-        "id": 123,
-        "status": "success"
-    }
+
+    response = {"message": "dream created successfully", "id": 123, "status": "success"}
 
     return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
