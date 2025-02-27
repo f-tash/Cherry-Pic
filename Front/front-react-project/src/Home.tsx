1eb0b67239286d01be810b1fddf30bae85612451
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-28 00:43:26 +0900

    退避

--- Front/front-react-project/src/Home.tsx
+++ Front/front-react-project/src/Home.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import Button from "@mui/material/Button";
-
+import { Box, Button, Typography } from "@mui/material";
+import "./style.css";
 const Home: React.FC = () => {
   return (
-    <div>
-      Home Component
-      <Button >aaaa</Button>
-    </div>
+    <Box>
+      <Typography
+        fontSize={40}
+        fontWeight={"bold"}
+        sx={{
+          fontFamily: "Noto Serif JP, serif",
+          fontOpticalSizing: "auto",
+          fontWeight: "normal",
+          fontStyle: "normal",
+        }}
+      >
+        Cherry-Pic
+      </Typography>
+      <Typography>心の中の夢が目の前の絵になる。</Typography>
+      <Typography>
+        Cherry-Picは入力されたあなたの夢を元にaiによって絵を作成します。早速あなたの夢を描きましょう！
+      </Typography>
+      <Button>試してみる</Button>
+    </Box>
   );
 };
 
