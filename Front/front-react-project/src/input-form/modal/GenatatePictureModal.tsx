4b1990b66f1144b908a274ac27561f7903f8d410
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-27 11:03:57 +0900

    feat: モーダルを作成

---
+++ Front/front-react-project/src/input-form/modal/GenatatePictureModal.tsx
@@ -0,0 +1,75 @@
+import { Box, Button, Modal, Typography } from "@mui/material";
+import React from "react";
+
+type GeneratePictureModalProps = {
+  dreamTitle: string;
+};
+
+const GeneratePictureModal: React.FC<GeneratePictureModalProps> = ({
+  dreamTitle,
+}) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <Box>
+      <Button
+        onClick={handleOpen}
+        variant="contained"
+        sx={{
+          bgcolor: "#FF75B6",
+          color: "white",
+          paddingX: 5,
+          borderRadius: 15,
+          marginTop: 5,
+        }}
+      >
+        画像を生成する
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          flexDirection={"column"}
+          borderRadius={3}
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="background.paper"
+          boxShadow={24}
+          py={7}
+          sx={{ transform: "translate(-50%, -50%)" }}
+        >
+          <Typography>画像が生成されました！</Typography>
+          <img
+            src="./ai-picture.png"
+            alt="仮の画像"
+            style={{ marginTop: 30 }}
+          />
+          <Typography sx={{ mt: 5 }}>あなたの夢：{dreamTitle}</Typography>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#FF75B6",
+              color: "white",
+              paddingX: 5,
+              borderRadius: 15,
+              marginTop: 5,
+            }}
+          >
+            投稿する
+          </Button>
+        </Box>
+      </Modal>
+    </Box>
+  );
+};
+
+export default GeneratePictureModal;
