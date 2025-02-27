3ee47668a354582e39e45188b543d3b5268a28f4
f-tash <f.dash.tash@gmail.com>
2025-02-27 16:38:24 +0900

    error handling for api add 400 error

--- Back/app.py
+++ Back/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, jsonify
 from api.dreams import get_dreams_from_db, post_dreams_to_db, submit_dream
 from api.validation import validate_post_dreams, validate_update_dreams
 
-
+from returns.pipeline import is_successful
 # Flaskの設定
 app = Flask(__name__)
 
@@ -17,8 +17,13 @@ def home():
 @app.route("/dreams", methods=["GET"])
 def get_dreams():
     limit = request.args.get("limit", 10)
-    response = get_dreams_from_db(limit=limit)
-    return jsonify(response)
+    res = get_dreams_from_db(limit=limit)
+
+    if not is_successful(res):
+        return jsonify({"error": "Internal Server Error"}), 500
+    
+    dreams = res.unwrap()
+    return jsonify(dreams)
 
 
 @app.route("/dreams", methods=["POST"])
@@ -29,18 +34,25 @@ def post_dream():
         return jsonify({"error": "Bad Request"}), 400
     dream_title = data["dream_title"]
 
-    img_url=post_dreams_to_db(dream_title)
+    res=post_dreams_to_db(dream_title)
+    if not is_successful(res):
+        return jsonify({"error": "Internal Server Error"}), 500
+    id,url=res.unwrap()
+    return jsonify(response={"dream_id":id,"url": url})
 
-    return jsonify(response={"url": img_url})
-
-@app.route("/dreams", methods=["UPDATE"])
+@app.route("/dreams", methods=["PATCH"])
 def update_dream():
     data = request.json
     is_request_ok = validate_update_dreams(data)
     if not is_request_ok:
         return jsonify({"error": "Bad Request"}), 400
-    id = data["id"]
-    submit_dream(id)
+    id = data["dream_id"]
+    submit = data["submit"]
+
+    res = submit_dream(id,submit)
+
+    if not is_successful(res):
+        return jsonify({"error": "Internal Server Error"}), 500
     
     return jsonify(response={"message": "Dream submitted successfully."})
 
