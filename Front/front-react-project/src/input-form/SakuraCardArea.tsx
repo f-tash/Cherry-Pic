0680e9657b03275d0a4d11c1c7928a093641b087
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-27 11:59:53 +0900

    refactor: SakuraCardArea

--- Front/front-react-project/src/input-form/SakuraCardArea.tsx
+++ Front/front-react-project/src/input-form/SakuraCardArea.tsx
@@ -1,33 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import OtherUserPost from "./OtherUserSakuraCard.tsx";
 import InputSakuraCardItem from "./InputSakuraCardItem.tsx";
+import { PhotoInfo } from "../Photo-list-type.ts";
 
 const SakuraCardArea = ({ value, setValue }) => {
+  const [photosInfoList, setPhotosInfoList] = useState<PhotoInfo[]>([]);
+
+  useEffect(() => {
+    setPhotosInfoList([
+      { id: 1, title: "大富豪になる", url: "https://via.placeholder.com/150" },
+      {
+        id: 2,
+        title: "野球選手になりたい",
+        url: "https://via.placeholder.com/150",
+      },
+      {
+        id: 3,
+        title: "仮面ライダーになる",
+        url: "https://via.placeholder.com/150",
+      },
+      {
+        id: 4,
+        title: "世界一周旅行クルーズ",
+        url: "https://via.placeholder.com/150",
+      },
+      {
+        id: 5,
+        title: "お客さんも自分も笑顔に幸せにできるヘアメイクさんになる!!",
+        url: "https://via.placeholder.com/150",
+      },
+      { id: 6, title: "カフェを開く", url: "https://via.placeholder.com/150" },
+      { id: 7, title: "猫カフェ巡り", url: "https://via.placeholder.com/150" },
+      {
+        id: 8,
+        title: "スポーツカーを買う",
+        url: "https://via.placeholder.com/150",
+      },
+      {
+        id: 9,
+        title:
+          "人生楽しくやりたいことをやって自分で幸せになれるように頑張ります。カラーが得意な美容師さんを目指します。",
+        url: "https://via.placeholder.com/150",
+      },
+      { id: 10, title: "ゲームを作る", url: "https://via.placeholder.com/150" },
+    ]);
+    // 仮の写真情報をセット
+  }, []);
+
+  if (photosInfoList.length === 0) {
+    return <Typography>Loading...</Typography>;
+  }
   const dreams = [
-    { gridColumn: 2, gridRow: 1, dream: "大富豪になる！" },
-    { gridColumn: 3, gridRow: 1, dream: "世界一周する！" },
-    { gridColumn: 4, gridRow: 1, dream: "プログラマーになる！" },
+    { gridColumn: 2, gridRow: 1, dream: photosInfoList[0].title },
+    { gridColumn: 3, gridRow: 1, dream: photosInfoList[1].title },
+    { gridColumn: 4, gridRow: 1, dream: photosInfoList[2].title },
     {
       gridColumn: 1,
       gridRow: 2,
-      dream: "お客さんも自分も笑顔に幸せにできるヘアメイクさんになる!!",
+      dream: photosInfoList[3].title,
     },
     {
       gridColumn: 2,
       gridRow: 2,
-      dream:
-        "人生楽しくやりたいことをやって自分で幸せになれるように頑張ります。カラーが得意な美容師さんを目指します。",
+      dream: photosInfoList[4].title,
     },
-    { gridColumn: 4, gridRow: 2, dream: "幸せになる" },
-    { gridColumn: 5, gridRow: 2, dream: "お店を出す" },
-    { gridColumn: 2, gridRow: 3, dream: "留学して英語をマスターする" },
+    { gridColumn: 4, gridRow: 2, dream: photosInfoList[5].title },
+    { gridColumn: 5, gridRow: 2, dream: photosInfoList[6].title },
+    { gridColumn: 2, gridRow: 3, dream: photosInfoList[7].title },
     {
       gridColumn: 3,
       gridRow: 3,
-      dream: "フロントもバックもできるフルスタックなエンジニアになる！",
+      dream: photosInfoList[8].title,
     },
-    { gridColumn: 4, gridRow: 3, dream: "洋服をたくさん買う" },
+    { gridColumn: 4, gridRow: 3, dream: photosInfoList[9].title },
   ];
 
   return (
