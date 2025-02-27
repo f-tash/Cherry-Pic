70dad95de18fe1e871572021affabd9a871df962
f-tash <f.dash.tash@gmail.com>
2025-02-28 01:26:06 +0900

    update Home title background

--- Front/front-react-project/src/Home.tsx
+++ Front/front-react-project/src/Home.tsx
@@ -3,22 +3,64 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home Component</h1>
-     
-
+    <div
+      style={{
+        position: "fixed",
+        display: "flex",
+        flexDirection: "column",
+        
+        alignItems: "center",
+        backgroundImage: "url('/background.jpeg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        minWidth: "100vw",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ fontFamily: "'Pacifico', cursive" ,textShadow: "2px 2px 4px #ffffff", color: "Black", fontSize: "48px", marginTop: "20vh" }}>
+        Cherry Pic
+      </h1>
       <nav>
-        <ul>
+        <ul style={{ listStyle: "none", padding: 0 }}>
           <li>
-            <Link to="/photo-list">Go to Photo List</Link>
+            <Link
+              to="/photo-list"
+              style={{
+                display: "block",
+                fontSize: "24px",
+                padding: "20px",
+                margin: "10px 0",
+                backgroundColor: "rgba(0, 0, 0, 0.7)",
+                color: "white",
+                textDecoration: "none",
+                borderRadius: "10px",
+              }}
+            >
+              Go to Photo Page
+            </Link>
           </li>
           <li>
-            <Link to="/input">Go to Input Page</Link>
+            <Link
+              to="/input"
+              style={{
+                display: "block",
+                fontSize: "24px",
+                padding: "20px",
+                margin: "10px 0",
+                backgroundColor: "rgba(0, 0, 0, 0.7)",
+                color: "white",
+                textDecoration: "none",
+                borderRadius: "10px",
+              }}
+            >
+              Go to Input Page
+            </Link>
           </li>
         </ul>
       </nav>
     </div>
   );
-};
+}
 
 export default Home;
