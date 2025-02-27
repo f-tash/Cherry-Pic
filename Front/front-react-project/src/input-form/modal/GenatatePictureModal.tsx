163aba437ff7c96a84eb646b770ac98899a7d739
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-27 19:52:18 +0900

    feat: postのapiを叩いて生成した画像を取得できるようにした

--- Front/front-react-project/src/input-form/modal/GenatatePictureModal.tsx
+++ Front/front-react-project/src/input-form/modal/GenatatePictureModal.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Modal, Typography } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import { z } from "zod";
-import React from "react";
+import { set, z } from "zod";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 type GeneratePictureModalProps = {
@@ -15,91 +15,61 @@ const GeneratePictureModal: React.FC<GeneratePictureModalProps> = ({
   setError,
   dreamTitle,
 }) => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [url, setUrl] = useState("");
+  const endpoint = "https://cherry-pic.onrender.com/dreams"; // APIのエンドポイント
+  const [loading, setLoading] = useState(false); // ローディング状態管理
   const schema = z
     .string()
     .min(1, "夢は1文字以上入力してください")
     .max(60, "夢は最大60文字までです");
 
-  const handleOpen = () => {
+  const handlePost = async () => {
+    //バリデーションチェック
     const result = schema.safeParse(value);
     if (!result.success) {
       setError(result.error.errors[0].message);
     } else {
       setError(""); // エラーなし
-      setOpen(true);
+      try {
+        setLoading(true); // ローディング開始
+        const response = await fetch(endpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            dream_title: value,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTPエラー: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log("成功:", result);
+        console.log("url", result.response.url);
+        setUrl(result.response.url);
+        setOpen(true);
+      } catch (error) {
+        console.error("エラー:", error);
+      } finally {
+        setLoading(false); // ローディング終了
+      }
     }
   };
   const handleClose = () => setOpen(false);
   const navigate = useNavigate();
   return (
     <Box>
-      <Button
-        onClick={handleOpen}
-        variant="contained"
-        sx={{
-          bgcolor: "#FF75B6",
-          color: "white",
-          paddingX: 5,
-          borderRadius: 15,
-          marginTop: 5,
-        }}
-      >
-        画像を生成する
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          flexDirection={"column"}
-          borderRadius={3}
-          position="absolute"
-          top="50%"
-          left="50%"
-          margin={"auto"}
-          width={{ xs: "85%", md: 400 }}
-          maxWidth={400}
-          bgcolor="background.paper"
-          boxShadow={24}
-          py={7}
-          sx={{ transform: "translate(-50%, -50%)" }}
-        >
+      {loading ? (
+        <Typography>画像を生成しています...</Typography>
+      ) : (
+        <>
           <Button
-            onClick={handleClose}
-            disableRipple
-            sx={{
-              borderRadius: "50%",
-              width: "48px",
-              height: "48px",
-              padding: "0",
-              minWidth: "0",
-              position: "absolute",
-              top: -20,
-              left: -20,
-              zIndex: 2,
-              bgcolor: "#FF75B6",
-            }}
-          >
-            <CloseIcon sx={{ color: "white" }}></CloseIcon>
-          </Button>
-          <Typography>画像が生成されました！</Typography>
-          <img
-            src="./ai-picture.png"
-            alt="仮の画像"
-            style={{ marginTop: 30 }}
-          />
-          <Typography sx={{ mt: 5, mx: 10 }}>
-            あなたの夢：{dreamTitle}
-          </Typography>
-          <Button
+            onClick={handlePost}
             variant="contained"
-            onClick={() => navigate("/photo-list")}
             sx={{
               bgcolor: "#FF75B6",
               color: "white",
@@ -108,10 +78,77 @@ const GeneratePictureModal: React.FC<GeneratePictureModalProps> = ({
               marginTop: 5,
             }}
           >
-            投稿する
+            画像を生成する
           </Button>
-        </Box>
-      </Modal>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              flexDirection={"column"}
+              borderRadius={3}
+              position="absolute"
+              top="50%"
+              left="50%"
+              margin={"auto"}
+              width={{ xs: "85%", md: 400 }}
+              maxWidth={400}
+              bgcolor="background.paper"
+              boxShadow={24}
+              py={7}
+              sx={{ transform: "translate(-50%, -50%)" }}
+            >
+              <Button
+                onClick={handleClose}
+                disableRipple
+                sx={{
+                  borderRadius: "50%",
+                  width: "48px",
+                  height: "48px",
+                  padding: "0",
+                  minWidth: "0",
+                  position: "absolute",
+                  top: -20,
+                  left: -20,
+                  zIndex: 2,
+                  bgcolor: "#FF75B6",
+                }}
+              >
+                <CloseIcon sx={{ color: "white" }}></CloseIcon>
+              </Button>
+              <Typography>画像が生成されました！</Typography>
+              <img
+                src={url}
+                alt="生成された画像"
+                style={{ marginTop: 30, borderRadius: 10 }}
+                width={250}
+                height={250}
+              />
+              <Typography sx={{ mt: 5, mx: 10 }}>
+                あなたの夢：{dreamTitle}
+              </Typography>
+              <Button
+                variant="contained"
+                onClick={() => navigate("/photo-list")}
+                sx={{
+                  bgcolor: "#FF75B6",
+                  color: "white",
+                  paddingX: 5,
+                  borderRadius: 15,
+                  marginTop: 5,
+                }}
+              >
+                投稿する
+              </Button>
+            </Box>
+          </Modal>
+        </>
+      )}
     </Box>
   );
 };
