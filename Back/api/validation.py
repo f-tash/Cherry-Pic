442e02e32d037f55ac8b329c865731807a318c4a
Keisuke Kimura <kimurakeisukej@gmail.com>
2025-02-27 16:39:25 +0900

    refactor: バリデーション

--- Back/api/validation.py
+++ Back/api/validation.py
@@ -14,13 +14,7 @@ def validate_post_dreams(data):
 def validate_update_dreams(data):
     # idが含まれているかの確認
     dream_id = data["dream_id"]
-    if dream_id is None:
-        return False
-
-    # submitがTrueかの確認
-    if "submit" not in data:
-        return False
-    if data["submit"] != True:
+    if dream_id is None or "submit" not in data or data["submit"] != True:
         return False
 
     return True
