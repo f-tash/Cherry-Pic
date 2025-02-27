10bd6474cd91779ee213a08df962be2e1f0644fd
f-tash <f.dash.tash@gmail.com>
2025-02-27 16:37:39 +0900

    using result type for api

--- Back/api/dreams.py
+++ Back/api/dreams.py
@@ -1,38 +1,54 @@
-from db.connect import post_dreams, update_img_url, get_dreams, submit_dream
+from db.connect import post_dreams, update_img_url, get_dreams, submit_dream_db
 from db.save_image import save_image
 from api.image_generate import image_generation
 
-def get_dreams_from_db(limit:int = 10):
-    """
-    Returns a list of dreams from the database.
-    """
-    dreams = get_dreams(limit)
-    return dreams
+from returns.result import Result, Success, Failure
 
-def post_dreams_to_db(dream_title:str):
+def get_dreams_from_db(limit:int = 10)-> Result[list, Exception]:
+    """
+    Gets dreams from the database.
+
+    Args:
+        limit (int, optional): The number of dreams to get. Defaults to 10.
+    
+    Returns:
+        Result[list, Exception]: Return Success(dreams) if the fetching was successful, Failure otherwise.
+    """
+    return get_dreams(limit)
+
+def post_dreams_to_db(dream_title:str) -> Result[tuple[int,str], Exception]:
     """
     Posts a dream to the database.
 
     Args:
         dream_title (str): The title of the dream.
     Returns:
-        img_url (str): The public URL of the uploaded image.
+        Result[tuple[int,str], Exception]: Return Success(dream_ud, image url) if the posting was successful, Failure otherwise.
     """
     default_url = "https://example.com" #todo: change this to some image url
     
-    id = post_dreams(dream_title,default_url)
-    file_path = image_generation(dream_title)
-    img_url = save_image(id,file_path)
-    update_img_url(id,img_url)
-    return img_url
+    try:
+        id = post_dreams(dream_title,default_url).unwrap()
+        file_path = image_generation(dream_title)
+        img_url = save_image(id,file_path).unwrap()
+        update_img_url(id,img_url)
+        return Success((id,img_url))
+    except Exception as e:
+        return Failure(e)
 
-def submit_dream(dream_id:int):
+
+def submit_dream(dream_id:int,submit:bool)->Result[None,Exception]:
     """
     Submits a dream to the database.
 
     Args:
         dream_id (int): The id of the dream.
+        submit (bool): Whether the dream is submitted or not.
     Returns:
-        None
+        Result[None, Exception]: Success if the submission was successful, Failure otherwise.
     """
-    submit_dream(dream_id)
+    try:
+        submit_dream_db(dream_id,submit)
+        return Success(None)
+    except Exception as e:
+        return Failure(e)
