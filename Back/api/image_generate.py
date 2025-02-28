d7695be3369420bec861f5b136f21f415191b4b3
f-tash <97144100+f-tash@users.noreply.github.com>
2025-02-28 11:20:59 +0900

    Revert "fix: モデルをdall-e-2からdall-e-3に更新"

--- Back/api/image_generate.py
+++ Back/api/image_generate.py
@@ -4,7 +4,6 @@ import base64
 
 from pydantic import BaseModel
 
-
 # 画像を生成する関数し、tmpファイルに保存してそのパスを返す
 def image_generation(title):
     prompt = create_prompt(title)
@@ -12,7 +11,7 @@ def image_generation(title):
         client = get_client()
         # 画像生成
         response = client.images.generate(
-            model="dall-e-3",  # モデル選択
+            model="dall-e-2",  # モデル選択
             prompt=prompt,  # プロンプト
             n=1,  # 生成数
             size="512x512",  # 解像度
@@ -28,25 +27,27 @@ def image_generation(title):
     except Exception as e:
         raise Exception(f"An error occurred during image generation: {str(e)}")
 
-
 def create_prompt(title):
-    try:
+    try :
         client = get_client()
         response = client.beta.chat.completions.parse(
             model="gpt-4o-mini",
             messages=[
                 {
-                    "role": "developer",
-                    "content": "Given the description of a dream someone wants to achieve, generate an appropriate prompt in English for image generation by dall-e-2 that best represents the dream.",
+                    "role": "developer", 
+                    "content": "Given the description of a dream someone wants to achieve, generate an appropriate prompt in English for image generation by dall-e-2 that best represents the dream."
                 },
-                {"role": "user", "content": title},
+                {
+                    "role": "user", 
+                    "content": title
+                }
             ],
-            response_format=PromptGeneratingImage,
+            response_format=PromptGeneratingImage
         )
         return response.choices[0].message.parsed.prompt_to_dalle
     except Exception as e:
         raise Exception(f"An error occurred during prompt generation: {str(e)}")
-
+    
 
 class PromptGeneratingImage(BaseModel):
     title: str
